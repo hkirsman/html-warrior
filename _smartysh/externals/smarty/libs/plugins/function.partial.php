fcdf18d7f72bc2b7e4046956a15fe548478ee798
Hannes Kirsman <hkirsman@gmail.com>
2011-04-13 07:31:27 +0000

    nicer code trepping

--- _smartysh/externals/smarty/libs/plugins/function.partial.php
+++ _smartysh/externals/smarty/libs/plugins/function.partial.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * Smarty plugin
  * -------------------------------------------------------------
@@ -8,86 +9,87 @@
  * Purpose:  include partial and assing parameters
  * -------------------------------------------------------------
  */
-function smarty_function_partial($params, &$smarty)
-{
-		global $smarty, $config;
 
-		$site_dir = explode("/", $smarty->template_dir);
-		$site_dir = $site_dir[0];
+function smarty_function_partial($params, &$smarty) {
+    global $smarty, $config;
 
-		if ( isset($params["tpl"]) ) { 
-			$params["template"] = $params["tpl"];
-		}
+    $site_dir = explode("/", $smarty->template_dir);
+    $site_dir = $site_dir[0];
 
-    foreach($params as $key=>$var) {
-			if($key != "template") {
-				$smarty->assign($key, $var);
-			}
-		}
+    if (isset($params["tpl"])) {
+        $params["template"] = $params["tpl"];
+    }
 
-		if ( $params["showcss"] ) { 
-			echo '<script type="text/javascript" src="http://www.google.com/jsapi"></script>';
-			echo '<script type="text/javascript" src="'.$config["path_code"].'/core/js/general.js"></script>';
-			echo '<script type="text/javascript">var partialName = "'.$params["template"].'";</script>';
-			echo '<script type="text/javascript" src="'.$config["path_code"].'/core/js/showcss.js"></script>';
-		}
+    foreach ($params as $key => $var) {
+        if ($key != "template") {
+            $smarty->assign($key, $var);
+        }
+    }
 
-		// copy template from code to site if template does not exist
-		if(!file_exists($config["basepath"]."/".$smarty->template_dir."partials/".$params["template"].".tpl")) { 
-			echo '<div style="background: red">Templatet ei ole olemas. Kopeerin uue?. <a href="?copy=yes">jah</a></div>';
-			if(@$_GET["copy"]=="yes") { 
-				if ( copy ( $config["code_path"]."/templates/partials/".$params["template"].".tpl" , $config["basepath"]."/".$smarty->template_dir."partials/".$params["template"].".tpl" ) ) {
-					echo "done";
-				} else { 
-					echo  $config["code_path"]."/templates/partials/".$params["template"].".tpl does not exist!";
-				}
-			}
-		}
-		$output = $smarty->fetch("partials/".$params["template"].".tpl");
+    if ($params["showcss"]) {
+        echo '<script type="text/javascript" src="http://www.google.com/jsapi"></script>';
+        echo '<script type="text/javascript" src="' . $config["path_code"] . '/core/js/general.js"></script>';
+        echo '<script type="text/javascript">var partialName = "' . $params["template"] . '";</script>';
+        echo '<script type="text/javascript" src="' . $config["path_code"] . '/core/js/showcss.js"></script>';
+    }
 
-		$page_variables = parse_variables($output);
-		$output = remove_variables($output);
+    // copy template from code to site if template does not exist
+    if (!file_exists($config["basepath"] . "/" . $smarty->template_dir . "partials/" . $params["template"] . ".tpl")) {
+        echo '<div style="background: red">Templatet ei ole olemas. Kopeerin uue?. <a href="?copy=yes">jah</a></div>';
+        if (@$_GET["copy"] == "yes") {
+            if (copy($config["code_path"] . "/templates/partials/" . $params["template"] . ".tpl", $config["basepath"] . "/" . $smarty->template_dir . "partials/" . $params["template"] . ".tpl")) {
+                echo "done";
+            } else {
+                echo $config["code_path"] . "/templates/partials/" . $params["template"] . ".tpl does not exist!";
+            }
+        }
+    }
+    $output = $smarty->fetch("partials/" . $params["template"] . ".tpl");
 
-		// copy files if these are listed in template
-		if(@$_GET["copy"]=="yes") {
-			if ( $page_variables["_files"] ) {
-				foreach($page_variables["_files"] as $key=>$var ) { 
-					if(file_exists($config["code_path"]."/".$var) && !file_exists($config["basepath"]."/".$site_dir ."/".$var) ) { 
-						if ( copy ( $config["code_path"]."/".$var , $config["basepath"]."/".$site_dir ."/".$var ) ) {
-							echo "done copying $var";
-						}
-					}
-				}
-			}
-			if ( $page_variables["stylefile"] ) {
-				// todo?
-			}
-			// get help if exists? todo
-		}
+    $page_variables = parse_variables($output);
+    $output = remove_variables($output);
 
-		// reset vars
-		foreach($params as $key=>$var) {
-			if($key != "template" && $key != "indent") {
-				$smarty->clearAssign($key);
-			}
-		}
-		$a_output = explode("\n", $output);
-		$first_line = true;
-		foreach($a_output as $key=>$var) { 
-			if(!$first_line || $params["fullindent"] ) {
-				$a_output[$key] = $params["indent"].$var;
-			} else { 
-				$first_line = false;
-				$a_output[$key] = $var;
-			}
-		}
-		// fix: remove lines with only spaces
-		$a_outputFinal = array();
-		foreach($a_output as $key=>$var) { 
-			if ( trim($var) != "" ) { 
-				$a_outputFinal[$key] = $var;
-			}
-		}
-		return implode("\n", $a_outputFinal);
+    // copy files if these are listed in template
+    if (@$_GET["copy"] == "yes") {
+        if ($page_variables["_files"]) {
+            foreach ($page_variables["_files"] as $key => $var) {
+                if (file_exists($config["code_path"] . "/" . $var) && !file_exists($config["basepath"] . "/" . $site_dir . "/" . $var)) {
+                    if (copy($config["code_path"] . "/" . $var, $config["basepath"] . "/" . $site_dir . "/" . $var)) {
+                        echo "done copying $var";
+                    }
+                }
+            }
+        }
+        if ($page_variables["stylefile"]) {
+            // todo?
+        }
+        // get help if exists? todo
+    }
+
+    // reset vars
+    foreach ($params as $key => $var) {
+        if ($key != "template" && $key != "indent") {
+            $smarty->clearAssign($key);
+        }
+    }
+    $a_output = explode("\n", $output);
+    $first_line = true;
+    foreach ($a_output as $key => $var) {
+        if (!$first_line || $params["fullindent"]) {
+            $a_output[$key] = $params["indent"] . $var;
+        } else {
+            $first_line = false;
+            $a_output[$key] = $var;
+        }
+    }
+    // fix: remove lines with only spaces
+    $a_outputFinal = array();
+    foreach ($a_output as $key => $var) {
+        if (trim($var) != "") {
+            $a_outputFinal[$key] = $var;
+        }
+    }
+    return implode("\n", $a_outputFinal);
 }
+
 ?>
