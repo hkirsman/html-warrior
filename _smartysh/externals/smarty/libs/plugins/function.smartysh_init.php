b57f6cad4ec9ea76a0e83d91bc17833d5bdfbd17
Hannes Kirsman <hkirsman@gmail.com>
2011-05-24 09:23:28 +0000

    + smartysh init code for templates. Simpler updates now possible.

---
+++ _smartysh/externals/smarty/libs/plugins/function.smartysh_init.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * Smarty plugin
+ * -------------------------------------------------------------
+ * File:     function.smartysh_init.php
+ * Type:     function
+ * Name:     smartysh_init
+ * Purpose:  include Smartysh plugins. Currently it only adds
+ * include code to html but does not incude files.
+ * -------------------------------------------------------------
+ */
+
+function smarty_function_smartysh_init($params, &$smarty) {
+    global $smarty, $config, $debug;
+
+    if ($smarty->getTemplateVars("debug") != 1) {
+        return "__smartysh_remove_line__";
+    }
+
+    $site_dir = explode("/", $smarty->template_dir);
+    $site_dir = $site_dir[0];
+
+    if (!isset($params["position"])) {
+        $params["position"] = "top";
+    }
+
+    $output = "";
+
+    if ($params["position"] == "top") {
+        $output .= html_javascript("../.." . $config["path_code"] . "/scripts/smartysh_init.php") . "\n";
+        $output .= html_javascript("../.." . $config["path_code"] . "/scripts/smartysh_helpers") . "\n";
+        $output .= '<link rel="stylesheet" type="text/css" href="' . $config["path_code"] . '/admin/style/_style_site.css" media="screen, projection, print" title="" />';
+    } elseif ($params["position"] == "bottom") {
+        $output .= html_javascript("../.." . $config["path_code"] . "/scripts/externals/jquery-ui") . "\n";
+        $output .= html_javascript("../.." . $config["path_code"] . "/scripts/externals/jquery.cookie") . "\n";
+        $output .= html_javascript("../.." . $config["path_code"] . "/scripts/psdOverlay") . "\n";
+    }
+
+    $a_output = explode("\n", $output);
+    $first_line = true;
+    foreach ($a_output as $key => $var) {
+        if (!$first_line) {
+            $a_output[$key] = $params["indent"] . $var;
+        } else {
+            $first_line = false;
+            $a_output[$key] = $var;
+        }
+    }
+
+    // fix: remove lines with only spaces
+    $a_outputFinal = array();
+    foreach ($a_output as $key => $var) {
+        if (trim($var) != "") {
+            $a_outputFinal[$key] = $var;
+        }
+    }
+
+    return implode("\n", $a_outputFinal);
+}
+
+?>
