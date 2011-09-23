d55d7370f7b76c41c0f3324675514b1c03f0db7c
Hannes Kirsman <hkirsman@gmail.com>
2011-09-23 23:40:44 +0300

    Disable favicon plugin and remove old code

--- _html-warrior/skeletons/default/cfg/config.php
+++ _html-warrior/skeletons/default/cfg/config.php
@@ -1,28 +1,13 @@
 <?php
+/*
+// We used to define every new page variable here
+// page variables are @foo = "" lines on the top of
+// templates/pages/foo.tpl
 if(isset($page_variables["hasLeftCol"]))
 	$smarty->assign("hasLeftCol", $page_variables["hasLeftCol"]);
 else
 	$smarty->assign("hasLeftCol", "");
-
-if(isset($page_variables["hasRightCol"]))
-	$smarty->assign("hasRightCol", $page_variables["hasRightCol"]);
-else
-	$smarty->assign("hasRightCol", "");
-
-if(isset($page_variables["isSubpage"]))
-	$smarty->assign("isSubpage", $page_variables["isSubpage"]);
-else
-	$smarty->assign("isSubpage", "");
-
-if(isset($page_variables["title2"]))
-	$smarty->assign("title2", $page_variables["title2"]);
-else
-	$smarty->assign("title2", "");
-
-if(isset($page_variables["yah"])) {
-	$smarty->assign("yah", explode(";", $page_variables["yah"]));
-} else
-	$smarty->assign("yah", "");
+*/
 
 
 $plugin = array();
@@ -33,8 +18,4 @@ $plugin[] = "jquery";
 #$plugin[] = "jquery.jcarousel";
 #$plugin[] = "jquery.nivo.slider";
 #$plugin[] = "jquery.swfupload";
-$plugin[] = "favicon";
-
-
-
-?>
+#$plugin[] = "favicon";
