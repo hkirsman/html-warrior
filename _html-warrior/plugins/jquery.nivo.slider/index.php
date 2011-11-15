39b1ad8cfcad5955983e27fd2647e2be9c12f34b
Hannes Kirsman <hkirsman@gmail.com>
2011-11-15 16:57:56 +0200

    Change external folder to externals

--- _html-warrior/plugins/jquery.nivo.slider/index.php
+++ _html-warrior/plugins/jquery.nivo.slider/index.php
@@ -1,7 +1,6 @@
 <?php
 
-$site_header = html_css("nivo-slider");
+$site_header = html_css('jquery.nivo-slider');
 
-$site_footer = html_javascript("externals/jquery.nivo.slider") . "\n";
-$site_footer .= html_javascript("jquery.nivo.slider.init") . "\n";
-?>
+$site_footer = html_javascript('externals/jquery.nivo.slider') . "\n";
+$site_footer .= html_javascript('jquery.nivo.slider.init') . "\n";
