8706397387f3611bde8f2f8b52714a2d4ca298a7
Hannes Kirsman <hkirsman@gmail.com>
2011-11-22 10:57:36 +0200

    Tested favicon and added some comments
    Also included type="image/x-icon" for ie favicon
    code

--- _html-warrior/plugins/favicon/index.php
+++ _html-warrior/plugins/favicon/index.php
@@ -1,7 +1,19 @@
 <?php
+/*
+  We add 2 codes to make favicon work: first for IE and second for all the other browsers.
+  This is tested on IE7-IE9, FF8, Chrome 15.0.874.121 m
+
+
+  http://en.wikipedia.org/wiki/ICO_%28file_format%29
+  For IE, the official IANA-registered MIME type for .ICO files is image/vnd.microsoft.icon.
+
+  When using the .ICO format for (X)HTML img elements, Internet Explorer versions 6 - 9b2
+  cannot display files served with the correct MIME type. A workaround is to use
+  the non-standard “image/x-icon” MIME type.
+*/
 
 $site_header = <<<EOF
-<link rel="shortcut icon" href="favicon.ico" />
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 <link rel="icon" href="favicon.png" type="image/png" />
 EOF;
 
